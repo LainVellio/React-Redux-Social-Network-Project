bc205fa0068ada22e849b554310ec407e45f5e96
LainVellio <cvellio@gmail.com>
2021-03-02 20:02:27 +0300

    git init

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://cdn2.iconfinder.com/data/icons/business-and-economy/256/business_economic_finance_interprise_company_rocket-256.png"></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"></img>
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New Post</div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
