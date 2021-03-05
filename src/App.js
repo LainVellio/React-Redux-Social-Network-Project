10e6fac5e52af35869b20579f85ddc19e300560e
LainVellio <cvellio@gmail.com>
2021-03-05 11:12:32 +0300

    add Lesson-18 add Dialogs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
@@ -8,6 +9,9 @@ const App = () => {
     <div className="app-wrapper">
       <Header />
       <Navbar />
+      <div className="app-wrapper-content">
+        <Dialogs />
+      </div>
       {/* <Profile /> */}
     </div>
   );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import classes from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className={classes.content}>
+    <div>
       <div>
         <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"></img>
       </div>
