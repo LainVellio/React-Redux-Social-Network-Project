6d95e2adc26f74487dcb5aa6f6daa4b9f2382c5c
LainVellio <cvellio@gmail.com>
2021-03-10 22:28:36 +0300

    add Lesson-27 component vs render

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,25 +3,11 @@ import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
 
 const Dialogs = (props) => {
-  const dialogsData = [
-    { id: 1, name: 'Dmitry' },
-    { id: 2, name: 'Alice' },
-    { id: 3, name: 'Sergey' },
-    { id: 4, name: 'Pavel' },
-    { id: 5, name: 'Vadim' },
-  ];
-
-  const dialogsElements = dialogsData.map((dialog) => (
+  const dialogsElements = props.dialogs.map((dialog) => (
     <DialogItem name={dialog.name} id={dialog.id} />
   ));
 
-  const messagesData = [
-    { id: 1, message: 'Hi' },
-    { id: 2, message: 'How are you?' },
-    { id: 3, message: 'I am fine' },
-  ];
-
-  const messagesElement = messagesData.map((message) => (
+  const messagesElement = props.messages.map((message) => (
     <Message message={message.message} />
   ));
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const dialogsData = [
+  { id: 1, name: 'Dmitry' },
+  { id: 2, name: 'Alice' },
+  { id: 3, name: 'Sergey' },
+  { id: 4, name: 'Pavel' },
+  { id: 5, name: 'Vadim' },
+];
+
+const messagesData = [
+  { id: 1, message: 'Hi' },
+  { id: 2, message: 'How are you?' },
+  { id: 3, message: 'I am fine' },
+];
+
+const postsData = [
+  { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+  { id: 2, message: "It's my first post", likesCount: 15 },
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App dialogs={dialogsData} messages={messagesData} posts={postsData} />
   </React.StrictMode>,
-  document.getElementById('root') 
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
